eef41d8cb38bf75bf2bc5c1a2808d578f40be877
hugo71119 <hugo71119@gmail.com>
2024-10-12 20:14:23 -0600

    proyecto web

---
+++ Program.cs
@@ -0,0 +1,7 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages(); // Add
+
+var app = builder.Build();
+// edit app.MapGet("/", () => "Hello World!");
+app.MapRazorPages(); 
+app.Run();
